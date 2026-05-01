563d549ec601fa255674b182d95c9eb8187f978e
7Okkotsu <lopezgarciasebas23@gmail.com>
2026-05-01 10:02:46 -0600

    Update qc.js

--- commands/stickers/qc.js
+++ commands/stickers/qc.js
@@ -27,7 +27,7 @@ export default {
       const name = user.name || m.sender.split('@')[0];
       const meta1 = user.metadatos ? String(user.metadatos).trim() : '';
       const meta2 = user.metadatos2 ? String(user.metadatos2).trim() : '';
-      let texto1 = meta1 ? meta1 : 'ʏᴜᴋɪ 🧠 Wᴀʙᴏᴛ';
+      let texto1 = meta1 ? meta1 : '𝓚𝓪𝓸𝓻𝓾𝓴𝓸';
       let texto2 = meta1 ? (meta2 ? meta2 : '') : `@${name}`;
       const tmpFile = `./tmp/qc-${Date.now()}.webp`;
       fs.writeFileSync(tmpFile, buffer);
